c0930c55315baf8784f3b76148db4c0a9e3a155d
myerraymond <myerraymond@y7mail.com>
2023-08-15 14:57:47 +1000

    updated

---
+++ public/navbar/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar/>
+    </React.Fragment>
+  );
+}
+
+export default App;
