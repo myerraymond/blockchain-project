0ccf3a713f416c2e857576fa3fd8d2a04b0d72d7
Rajarshi Sinha <69039308+Roitta11@users.noreply.github.com>
2023-08-30 19:54:32 +0600

    Update AboutPage.js
    the code has been modified to add the profile images of each people with the correct reference link

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,69 @@
+// import React from "react";
+// import "./about.css"; // Import the CSS file
+
+
+// const About = () => {
+
+//   return   <div className="container">
+//       <br />
+
+//       <div className="profiles">
+//         <div className="profile">
+//           {/* insert image here */}
+//           <img src="photo.JPG" className="profile-img" alt="Profile" />
+
+//           <h3 className="user-name">Myer</h3>
+//           <h5>FrontEnd Developer</h5>
+//           <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+//             consequatur, architecto delectus magni pariatur hic explicabo
+//             praesentium ipsa nisi asperiores suscipit animi.
+//           </p>
+//         </div>
+//         <div className="profile">
+//           {/* insert image here */}
+//           <img src="photo.JPG" className="profile-img" alt="Profile" />
+
+//           <h3 className="user-name">Sinha</h3>
+//           <h5>FrontEnd Developer</h5>
+//           <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+//             consequatur, architecto delectus magni pariatur hic explicabo
+//             praesentium ipsa nisi asperiores suscipit animi.
+//           </p>
+//         </div>
+//         <div className="profile">
+//           {/* insert image here */}
+//           <img src="photo.JPG" className="profile-img" alt="Profile" />
+
+//           <h3 className="user-name">Yasindu</h3>
+//           <h5>FrontEnd Developer</h5>
+//           <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+//             consequatur, architecto delectus magni pariatur hic explicabo
+//             praesentium ipsa nisi asperiores suscipit animi.
+//           </p>
+
+//           <a href="">
+//             <ion-icon name="logo-github"></ion-icon>
+//           </a>
+//         </div>
+//       </div>
+//     </div>;
+// };
+
+// export default About;
+
+
+
+// new code added here
+
 import React from "react";
 import "./about.css"; // Import the CSS file
+import myerImage from "./myer.png";
+import sinhaImage from "./sinha.png";
+import yasiImage from "./yasi.png";
+
 
 
 const About = () => {
@@ -10,7 +74,7 @@ const About = () => {
       <div className="profiles">
         <div className="profile">
           {/* insert image here */}
-          <img src="photo.JPG" className="profile-img" alt="Profile" />
+          <img src={myerImage} className="profile-img" alt="Profile" />
 
           <h3 className="user-name">Myer</h3>
           <h5>FrontEnd Developer</h5>
@@ -21,27 +85,24 @@ const About = () => {
           </p>
         </div>
         <div className="profile">
-          {/* insert image here */}
-          <img src="photo.JPG" className="profile-img" alt="Profile" />
+        {/* insert the image here */}
+          <img src={sinhaImage} className="profile-img" alt="Profile" />
 
           <h3 className="user-name">Sinha</h3>
-          <h5>FrontEnd Developer</h5>
+          <h5>BackEnd Developer</h5>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            consequatur, architecto delectus magni pariatur hic explicabo
-            praesentium ipsa nisi asperiores suscipit animi.
+           Ciao! I am Rajarshi Sinha, currently in my second year of Computer Science degree, pursuing a career in Data Science.  
+           Besides, my days are filled with activities such as soccer, basketball and also meeting friends. 
           </p>
         </div>
         <div className="profile">
           {/* insert image here */}
-          <img src="photo.JPG" className="profile-img" alt="Profile" />
+          <img src={yasiImage} className="profile-img" alt="Profile" />
 
           <h3 className="user-name">Yasindu</h3>
           <h5>FrontEnd Developer</h5>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            consequatur, architecto delectus magni pariatur hic explicabo
-            praesentium ipsa nisi asperiores suscipit animi.
+          Hey, I am a second year Computer Science student, studying at Swinburne University of Technology and I am majoring in Data Science. I love playing soccer, coding and watching F1 during my free time.
           </p>
 
           <a href="">
